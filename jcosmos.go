dee6f1eaaf4a726e01d01a874a7a9e2d49b9cda0
Jae <26292620+JaeTLDR@users.noreply.github.com>
2023-05-26 09:10:15 +1000

    add const for simulator key

--- jcosmos.go
+++ jcosmos.go
@@ -20,6 +20,7 @@ import (
 )
 
 const (
+	CosmosDBSimulatorKey            = "C2y6yDjf5/R+ob0N8A7Cgv30VRDJIWEHLM+4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw/Jw=="
 	JcosmosVersion                  = "0.0.2"
 	timeoutSeconds                  = 9
 	tokenVersion             string = "1.0"
